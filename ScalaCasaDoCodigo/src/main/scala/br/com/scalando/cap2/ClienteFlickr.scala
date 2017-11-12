ab0626baa13e07ddc43b7a4714fd2379c3875638
George Salu <george.salu10@gmail.com>
2017-11-12 15:56:47 -0200

    capitulo 2 - acessando fotos do flickr

---
+++ ScalaCasaDoCodigo/src/main/scala/br/com/scalando/cap2/ClienteFlickr.scala
@@ -0,0 +1,18 @@
+package br.com.scalando.cap2
+
+import java.util.Properties
+
+object ClienteFlickr extends App {
+	
+    val props = new Properties();
+    props.load(getClass.getClassLoader.getResourceAsStream("config.properties"));
+    
+    val apiKey = props.getProperty("flickr.api.key");
+    val method = "flickr.photos.search";
+    val tags = "scala";
+    
+    val	url	=	s"https://api.flickr.com/services/rest/?method=$method&api_key=$apiKey&tags=$tags"
+    
+    scala.io.Source.fromURL(url).getLines().foreach(println);
+    
+}
