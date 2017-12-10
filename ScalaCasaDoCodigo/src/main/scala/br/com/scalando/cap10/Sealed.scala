9b2cb69b4ac9cbcfdb0b7c82624e60809f990664
George Salu <george.salu10@gmail.com>
2017-12-09 22:58:08 -0200

    Adiciona exemplo de sealed class

---
+++ ScalaCasaDoCodigo/src/main/scala/br/com/scalando/cap10/Sealed.scala
@@ -0,0 +1,9 @@
+package br.com.scalando.cap10
+
+object Sealed extends App {
+    sealed abstract class Media(val value: String)
+
+    object Fotos extends Media("fotos")
+    object Videos extends Media("videos")
+    object Todas extends Media("all")
+}
