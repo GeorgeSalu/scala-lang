6cc3d4b2f57410c9aa53c6fd7402724146663a03
George Salu <george.salu10@gmail.com>
2017-12-16 21:01:06 -0200

    Adiciona exemplo de implicits com Specs2

---
+++ ScalaCasaDoCodigo/src/main/scala/br/com/scalando/cap12/ConflitoImplicits.scala
@@ -0,0 +1,11 @@
+package br.com.scalando.cap12
+
+import br.com.scalando.cap11.Foto
+
+object ConflitoImplicits extends App {
+    implicit def toPrintableFoto(foto: Foto): PrintableFoto = new PrintableFoto(foto)
+    implicit def newPrintableFoto(foto: Foto): PrintableFoto = new PrintableFoto(foto)
+
+    // descomente para ver o erro de compilação com implicits ambíguos
+    // Foto("foto1", "jcranky", 1234, "Foto 1", None).print
+}
