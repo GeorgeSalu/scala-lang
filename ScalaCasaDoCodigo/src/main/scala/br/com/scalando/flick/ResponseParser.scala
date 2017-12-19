86567fa67742e9d71eb4b559261d8aaa58664692
George Salu <george.salu10@gmail.com>
2017-12-19 15:51:09 -0200

    Adiciona implementação simples do parsing de xml de fotos

--- ScalaCasaDoCodigo/src/main/scala/br/com/scalando/flick/ResponseParser.scala
+++ ScalaCasaDoCodigo/src/main/scala/br/com/scalando/flick/ResponseParser.scala
@@ -8,9 +8,32 @@ sealed trait ResponseParser {
 }
 
 final class XmlFlickrParser extends ResponseParser {
-    override def parse(str: String): Seq[Foto] = Nil
+
+    import XmlFlickrParser._
+
+    // TOOD: proper erro handling in the parsing below
+    override def parse(xmlStr: String): Seq[Foto] =
+        (XML.loadString(xmlStr) \\ "photo").map { photoXml =>
+            Foto(
+                (photoXml \ "@id").text,
+                (photoXml \ "@owner").text,
+                (photoXml \ "@secret").text,
+                (photoXml \ "@server").text,
+                (photoXml \ "@farm").text.toInt,
+                (photoXml \ "@title").text,
+                calcBoolean((photoXml \ "@ispublic").text),
+                calcBoolean((photoXml \ "@isfriend").text),
+                calcBoolean((photoXml \ "@isfamily").text))
+        }
 }
 
 object XmlFlickrParser {
+    // TODO: write a test for this guy
+    def calcBoolean(rawAttribute: String): Boolean =
+        rawAttribute.toInt match {
+            case 1 => true
+            case _ => false
+        }
+
     def fromConfig(config: Config): XmlFlickrParser = new XmlFlickrParser()
 }
